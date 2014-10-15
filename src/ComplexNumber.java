b3c5037d22043a3aacc584a55e3ba6a5d0354c01
jacobjoh <jacobjoh@husky.neu.edu>
2014-10-14 22:24:45 -0700

    Adding DFT and complex number classes
    DFT has algorithm for dft. ComplexNumber is a class for complex numbers

---
+++ src/ComplexNumber.java
@@ -0,0 +1,25 @@
+public class ComplexNumber{
+
+	private double r;
+	private double i;
+	
+	public ComplexNumber(double r, double i){
+		this.r = r;
+		this.i = i;
+	}
+	
+	public ComplexNumber add(ComplexNumber c2){
+		return new ComplexNumber(this.r + c2.r, this.i + c2.i);
+	}
+	
+	public ComplexNumber multiply(ComplexNumber c2){
+		return new ComplexNumber(this.r*c2.r - this.i*c2.i, this.r*c2.i + this.i*c2.r);
+	}
+	
+	public ComplexNumber divide(ComplexNumber c2){
+		return new
+		 ComplexNumber((this.r*c2.r+this.i*c2.i)/(Math.pow(c2.r,2)+Math.pow(c2.i,2)),
+		 			(this.i*c2.r - this.r*c2.i)/(Math.pow(c2.r,2)+Math.pow(c2.i,2)));
+	}
+
+}
---
+++ src/DFT.java
@@ -0,0 +1,23 @@
+public class DFT{
+
+	//compute DFT
+	public static ComplexNumber[] dft(ComplexNumber[] input){
+	
+		int N = input.length;
+		ComplexNumber[] output = new ComplexNumber[N];
+		
+		for(int k=0; k<N; k++){
+			ComplexNumber c = new ComplexNumber(0,0);
+			output[k] = c;
+			for(int n=0; n<N; n++){
+				double real = Math.cos(-2*Math.PI*n*k/N);
+				double imaginary = Math.sin(-2*Math.PI*n*k/N);
+				output[k] = output[k].add(input[n].multiply(new ComplexNumber(real,imaginary)));
+			}
+		}
+		
+		return output;
+	
+	}
+	
+}
