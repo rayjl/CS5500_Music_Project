233a4ef30816ee975fe8c01fa83730f4f689b31e
jacobjoh <jacobjoh@husky.neu.edu>
2014-10-15 02:02:28 -0700

    added comments to ComplexNumber

--- src/ComplexNumber.java
+++ src/ComplexNumber.java
@@ -1,22 +1,38 @@
 public class ComplexNumber {
 
-	private double r;
-	private double i;
+	private double r; //real part
+	private double i; //imaginary part
+	
 	
 	public ComplexNumber(double r, double i){
 		this.r = r;
 		this.i = i;
 	}
 	
+	/* ComplexNumber -> ComplexNumber
+	 * Given: a ComplexNumber that represents a complex number
+	 * Returns: the result of adding this ComplexNumber with given
+	            input
+	 */
 	public ComplexNumber add(ComplexNumber c2){
 		return new ComplexNumber(this.r + c2.r, this.i + c2.i);
 	}
 	
+	/* ComplexNumber -> ComplexNumber
+	 * Given: a ComplexNumber that represents a complex number
+	 * Returns: the result of multiplying this ComplexNumber with given
+	            input
+	 */
 	public ComplexNumber multiply(ComplexNumber c2){
 		return new ComplexNumber(this.r*c2.r - this.i*c2.i, 
 				this.r*c2.i + this.i*c2.r);
 	}
 	
+	/* ComplexNumber -> ComplexNumber
+	 * Given: a ComplexNumber that represents a complex number
+	 * Returns: the result of dividing this ComplexNumber with given
+	            input
+	 */	
 	public ComplexNumber divide(ComplexNumber c2){
 		return new
 		 ComplexNumber((this.r*c2.r + this.i*c2.i) 
@@ -25,14 +41,26 @@ public class ComplexNumber {
 				 			/ (Math.pow(c2.r,2) + Math.pow(c2.i,2)));
 	}
 	
+	/*  -> String
+	 * Given: 
+	 * Returns: a string representation of this ComplexNumber
+	 */	
 	public String toString(){
 		return this.r + " + " + this.i + "i";
 	}
 	
+	/* -> double
+	 * Given:
+	 * Returns: the magnitude of this ComplexNumber 
+	 */	
 	public double getMagnitude(){
 		return Math.sqrt(i*i + r*r);
 	}
 	
+	/*  -> double
+	 * Given: 
+	 * Returns: the phase of this ComplexNumber
+	 */	
 	public double getFreq(){
 		if(r<0){
 			return Math.PI + Math.atan(i/r);
