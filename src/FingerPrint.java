f5c4d2f7ba24f52aea907abb480bf9de6736ee8d
Ray <rayjl@uw.edu>
2014-10-15 11:25:04 -0700

    Slight modifications

--- src/FingerPrint.java
+++ src/FingerPrint.java
@@ -1,36 +1,40 @@
 /* 
  * FingerPrint Object
- * -Contain the "FingerPrint" of an audio file
+ * -Contains the "FingerPrint" of an audio file
  * -Object is for storage purposes
  */
 
 public class FingerPrint {
 
 	// TODO - fix dummy threshold values of 5
-	private double FREQ_THRESHOLD = 5;
+	private double FREQ_THRESHOLD = 1;
 	private double MAG_THRESHOLD = 5;
 	private double magnitude;
 	private double freq;
 	
-	
-	public FingerPrint(ComplexNumber c){
+	// Constructor
+	public FingerPrint(ComplexNumber c) {
 		this.magnitude = c.getMagnitude();
 		this.freq = c.getFreq();	
 	}
 	
-	public boolean similarTo(FingerPrint f){
+	// FingerPrint -> boolean
+	// Given: compares this FingerPrint to the given FingerPrint
+	// Returns: true if the FingerPrints are "similar"
+	public boolean similarTo(FingerPrint f) {
+		// Check thresholds
 		if (Math.abs(magnitude - f.getMagnitude()) < this.MAG_THRESHOLD
 				&& Math.abs(freq - f.getFreq()) < this.FREQ_THRESHOLD)	
 			return true;
 		return false;
 	}
 	
-	
-	public double getFreq(){
+	// Getters --------------------------------------------
+	public double getFreq() {
 		return this.freq;
 	}
 	
-	public double getMagnitude(){
+	public double getMagnitude() {
 		return this.magnitude;
 	}
 	
