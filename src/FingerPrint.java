9878a087d1eca1267f3ab55087115691580702cb
Ray <rayjl@uw.edu>
2014-11-15 16:18:08 -0800

    changes

--- src/FingerPrint.java
+++ src/FingerPrint.java
@@ -23,6 +23,7 @@ public class FingerPrint {
     
     private double findMaxMagnitude(ComplexNumber[] window){
         double maxValue = 0;
+        System.out.println(window.length);
         System.out.println("entering for loop");
         for(int i = 0; i < window.length; i++){
             maxValue = Math.max(findPowerDensity(window[i].getMagnitude()), maxValue);
